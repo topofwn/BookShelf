7e06b03b06a5f6934f0500921ab8217b749ca7a6
Tuan Giao <thanhluan1451996@gmail.com>
2018-04-27 09:53:11 +0700

    update6

--- app/src/main/java/nhom1/nhom1/bookshelf/Book.java
+++ app/src/main/java/nhom1/nhom1/bookshelf/Book.java
@@ -1,4 +1,25 @@
 package nhom1.nhom1.bookshelf;
 
 public class Book {
+    public String Name;
+    public String Description;
+    public String Expired_date;
+    public long cost;
+
+    public String getName() {
+        return Name;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public String getExpired_date() {
+        return Expired_date;
+    }
+
+    public long getCost() {
+        return cost;
+    }
+
 }
